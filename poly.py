4a432cc5389d4f3afa3dcc2556b098d0d1483148
Grant Rosario <grantster1124@aol.com>
2017-02-18 23:19:52 -0500

    Initial Commit

---
+++ poly.py
@@ -0,0 +1,38 @@
+# Polynomial Regression
+
+# Importing the Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.set_printoptions(threshold=np.nan)
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2].values
+
+# Splitting data into training set and test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_Y = StandardScaler()
+Y_train = sc_Y.fit_transform(Y_train)"""
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, Y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, Y)
